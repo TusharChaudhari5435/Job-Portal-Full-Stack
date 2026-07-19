5e06feee0cabee0587aa8ab2856c57eb42145bab
Tushar Chaudhari <chaudharit887@gmail.com>
2026-07-19 17:05:54 +0530

    Fix import case for User model

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { User } from "../models/User.model.js";
+import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const register = async (req, res) => {
